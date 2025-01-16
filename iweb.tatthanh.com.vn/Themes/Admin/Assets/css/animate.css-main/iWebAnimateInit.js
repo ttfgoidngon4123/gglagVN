5fd044113ed6d3630ed0243f0f467b3e8daa1cd9
ttfgoidngon4123 <Khoisever12345@gmail.com>
2025-01-17 00:41:55 +0700

    Add files via upload

---
+++ iweb.tatthanh.com.vn/Themes/Admin/Assets/css/animate.css-main/iWebAnimateInit.js
@@ -0,0 +1,22 @@
+﻿$(window).scroll(function(){
+    $("[class*='wait_a_']").each(function () {
+        var animateName = GetAnimateName($(this).attr("class"));
+
+        if ($(this).offset().top + $(this).height() < $(window).scrollTop() || $(this).offset().top > $(window).scrollTop() + $(window).height()) {            
+            $(this).removeClass("animate__animated animate__" + animateName);
+        }
+        else {
+            $(this).addClass("animate__animated animate__" + animateName);
+        }
+    });
+});
+
+function GetAnimateName(classes) {
+    var s = "";
+    classes.split(' ').forEach(function (item) {
+        if (item.startsWith("wait_a_"))
+            s = item.replace("wait_a_", "");
+    });
+
+    return s;
+}
